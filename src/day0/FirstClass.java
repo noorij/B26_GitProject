9bc13a7ce694096bd0bd4cbbd8a7602bd22a5b20
jawid <jawidahmada4@gmail.com>
2022-03-27 09:58:15 -0400

    New code added to the class

--- src/day0/FirstClass.java
+++ src/day0/FirstClass.java
@@ -1,4 +1,13 @@
 package day0;
 
 public class FirstClass {
+    public static void main(String[] args) {
+        String str ="java";
+        String reverse = "";
+        for (int i = str.length()-1; i >= 0 ; i--) {
+            reverse += str.charAt(i);
+        }
+        System.out.println(reverse);
+    }
+
 }
--- src/day0/SecondClass.java
+++ src/day0/SecondClass.java
@@ -3,5 +3,6 @@ package day0;
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("This is class is added from IntelliJ");
+
     }
 }
